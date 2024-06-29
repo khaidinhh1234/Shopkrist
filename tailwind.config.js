01bc45c1997ff3c95c56f775195b82bba997a8ca
khaidinhh1234 <khaindph36466@fpt.edu.vn>
2024-06-29 19:05:13 +0700

    html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,html}"],
+  content: ["./index.html", "./src/**/*.{js,tsx,jsx,html}"],
   theme: {
     extend: {
       colors: {
         blackL: "#131118",
+        hrBlack: "#eaeaeb",
       },
       backgroundImage: {
         bannerL: "url('/src/assets/images/banner.png')",
